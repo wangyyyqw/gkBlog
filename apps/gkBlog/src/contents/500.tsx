dc18bd3d4501ea9a7ffa76c81b717d97b5110d3e
qlAD <qlad_adgk@163.com>
2025-09-09 18:44:17 +0800

    refactor(gkBlog): 优化错误页面信息

--- apps/gkBlog/src/contents/500.tsx
+++ apps/gkBlog/src/contents/500.tsx
@@ -4,18 +4,21 @@ function Error500Contents() {
   return (
     <div
       className={clsx(
-        "background-grid flex h-full items-center justify-center",
+        "background-grid flex h-full items-center justify-center"
       )}
     >
       <div
         className={clsx(
           "content-wrapper text-center text-slate-600",
-          "dark:text-slate-400",
+          "dark:text-slate-400"
         )}
       >
         <h1 className={clsx("py-12 text-center")}>
           <div className={clsx("mb-3 text-8xl font-extrabold")}>500</div>
-          <div className={clsx("text-2xl")}> 服务器错误 </div>
+          <div className={clsx("text-2xl")}>
+            {" "}
+            服务器错误，请耐心等待服务器同步{" "}
+          </div>
         </h1>
       </div>
     </div>
