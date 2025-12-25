0ae886afd7adf63c7f7bf31ab507c596aa6d29f2
蠢卷栖萤 <wanmei8672873@outlook.com>
2025-12-25 18:49:58 +0800

    feat: 将网站图标修改为 cover.jpg
    - 在 _document.tsx 中添加对 /assets/images/cover.jpg 的引用
    - 设置了 favicon 和 apple-touch-icon 为指定图片

--- apps/gkBlog/src/pages/_document.tsx
+++ apps/gkBlog/src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Head, Html, Main, NextScript } from "next/document";
 function Document() {
   return (
     <Html lang="zh-CN">
-      <Head />
+      <Head>
+        <link rel="icon" href="/assets/images/cover.jpg" type="image/jpg" />
+        <link rel="apple-touch-icon" href="/assets/images/cover.jpg" />
+      </Head>
       <body>
         <div id="skip-navigation" />
         <Main />
